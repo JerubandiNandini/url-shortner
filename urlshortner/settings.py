33b204d3f190604e7f26e14af76b78343a458247
JerubandiNandini <madhunandini7878@gmail.com>
2021-09-07 18:06:08 +0530

    First commit

--- urlshortner/settings.py
+++ urlshortner/settings.py
@@ -18,8 +18,8 @@ import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+# Quick-start developmenst/t settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checkli
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-(%8@pho^3)#2414(%i%-@krfjyx&n%x8z@7*b3jhxih27auj19'
